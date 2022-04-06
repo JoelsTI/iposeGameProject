2c00829ee08ce715fe0dc09b34069bbbaa106552
Sander <sblijleven97@gmail.com>
2022-04-06 15:59:25 +0200

    Mapsize en borders gefixt

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -41,6 +41,8 @@ public class Game extends GameApplication{
     protected void initSettings(GameSettings gameSettings) {
         gameSettings.setFullScreenFromStart(true);
         gameSettings.setFullScreenAllowed(true);
+        gameSettings.setWidth(15 * 70);
+        gameSettings.setHeight(10 * 70);
         gameSettings.setTitle("Javatar Game");
         gameSettings.setVersion("1.0");
     }
