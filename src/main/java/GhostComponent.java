e70f20e8f57a873bc01bc26bdcfe31a9dea0bb03
JoelsTI <74766789+JoelsTI@users.noreply.github.com>
2022-04-07 11:03:59 +0200

    Update GhostComponent.java

--- src/main/java/GhostComponent.java
+++ src/main/java/GhostComponent.java
@@ -1,6 +1,9 @@
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.component.Component;
 import com.almasb.fxgl.texture.Texture;
+import javafx.geometry.Rectangle2D;
+
+import java.util.Random;
 
 public class GhostComponent extends Component {
 
@@ -11,6 +14,10 @@ public class GhostComponent extends Component {
     private final Texture left;
     private final Texture rigth;
     private final Texture upDown;
+    private static final double SPEED = 100.0;
+    private double dx = 100.0;
+    private double dy = -SPEED;
+
 
     public GhostComponent(String name, double x,double y) {
         this.name = name;
@@ -23,5 +30,21 @@ public class GhostComponent extends Component {
     @Override
     public void onAdded() {
         entity.getViewComponent().addChild(upDown);
+    }
+
+    Random rand = new Random();
+    int upperbound =25;
+    int int_random = rand.nextInt(upperbound);
+
+    @Override
+    public void onUpdate(double tpf) {
+        if (entity.getX() < 100 ){
+            entity.translateX(dx * tpf);
+        }
+        else entity.translateX(-dx * tpf);
+        if(entity.getY() < 100){
+            entity.translateY(dy * tpf);
+        }
+        else entity.translateY(-dy * tpf);
     }
 }
